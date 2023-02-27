8cdec15b42a8956ef3f2278e98aa081d011cf1c6
Felipe Gamer <100796428+fetuber4095@users.noreply.github.com>
2023-02-27 10:00:34 -0300

    alpha c1.0.5 - Shell Implement
    Any bug fix and the first command was add

--- service.py
+++ service.py
@@ -3,13 +3,44 @@ from time import sleep
 from random import randint
 from getpass import getpass
 
-__version__ = ["alpha b1.0.0", "Kernel Implement"]
+import socket
+
+__version__ = ["alpha c1.0.5", "Shell Implement"]
+
+
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+hostname = socket.gethostname()    
+IPAddr = socket.gethostbyname(hostname)
 
 class index:
 	def __init__(self, user):
-		print("Comming Soon. . .")
-		input()
+		self.player = user
+		self.appname = "Time Terminal"
+		self.appdata = "settings.json"
+		self.index()
 
+	def index(self):
+		print(f"Welcome, {self.player} - {self.appname} [{__version__[0]} - {__version__[1]}]")
+		while True:
+			try:
+				cmd = input(f"{self.player}@{IPAddr}:~$ ").strip()
+				if cmd == "exit":
+					print("Goodbye. . .")
+					input("")
+					break
+
+
+
+				else:
+					cmd = cmd.split()
+					print(f"{cmd[0]}: Unknow Command Error")
+			except KeyboardInterrupt:
+					print("\nGoodbye. . .")
+					input("")
+					break
+
+					
 try:
 	x = open("settings.json")
 	x.close()
@@ -23,14 +54,14 @@ except FileNotFoundError:
 	user = x
 	password = x2
 	del x, x2, x3
-	index(x)
+	index(user)
 
 else:
 	x = open("settings.json", "rt").read()
-	x = x.slipt()
+	x = x.split()
 	while True:
 		x2 = getpass(f"{x[0]}, password: ")
-		if x2 = x[1]:
+		if x2 == x[1]:
 			index(x[0])
 			break
 		else:
